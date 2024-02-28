b5d481b2476629692c370c1dd19e27296cd215d1
makwerik <serezha0790@mail.ru>
2024-02-28 12:34:09 +0300

    class settings for game

---
+++ alien_invasion/settings.py
@@ -0,0 +1,15 @@
+
+class Settings:
+
+    """Класс для хранения всех настроек игры"""
+
+    def __init__(self):
+
+        """Настройки игры"""
+
+        self.screen_width = 1200
+        self.screen_height = 800
+        # Цвет окна
+        self.bg_color = (255, 255, 255)
+        # Название окна
+        self.name_window = 'Инопланетное вторжение'
