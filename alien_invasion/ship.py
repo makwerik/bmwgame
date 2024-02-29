8d4f4bb83ef554dbb9ca519bca3cc4c142b7b95e
makwerik <serezha0790@mail.ru>
2024-02-29 18:55:40 +0300

    add my car

---
+++ alien_invasion/ship.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Ship:
+
+    """Класс для управления ласточкой"""
+
+    def __init__(self, ai_game):
+        """
+        Принимает аргумент класс alien_invasion
+        Задает начальнгую позицию ласточке
+        """
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        """Загружаю изображение  и получаю прямоугольник"""
+        self.image = pygame.image.load('images/bmw.bmp')
+        self.rect = self.image.get_rect()
+
+        """Каждый новый бумер появляется у нижнего края экрана"""
+        self.rect.midbottom = self.screen_rect.midbottom
+
+
+    def blitme(self):
+        """Рисуем бумер"""
+        self.screen.blit(self.image, self.rect)
