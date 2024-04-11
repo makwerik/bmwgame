75063c72f7e93c22764547c60929b044d47f2e7a
makwerik <serezha0790@mail.ru>
2024-04-11 19:22:48 +0300

    рефакторинг

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -92,7 +92,11 @@ class AlienInvasion:
     #     Создаем первый ряд масла
         for alien_number in range(number_aliens_x):
     #         Создания масла и размещение его в ряду
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
             alien = Alien(self)
+            alien_width = alien.rect.width
             alien.x = alien_width + 2 * alien_width * alien_number
             alien.rect.x = alien.x
             self.aliens.add(alien)
